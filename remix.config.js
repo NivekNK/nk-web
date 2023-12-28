070f1b4c8d3ae24d5622dc403de11fa1f0565cee
NivekNK <kevin.rojas.v@mail.pucv.cl>
2023-12-28 02:53:03 -0300

    Initial commit

---
+++ remix.config.js
@@ -0,0 +1,13 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+    /*  Default config */
+    ignoredRouteFiles: ["**/.*"],
+    // appDirectory: "app",
+    // assetsBuildDirectory: "public/build",
+    // publicPath: "/build/",
+    // serverBuildPath: "build/index.js",
+
+    /*  shadcn-ui */
+    tailwind: true,
+    postcss: true,
+};
